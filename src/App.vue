296ee8c5802207df16b9242dc12f21b9a28562d7
jacobhsu <jacob.hsu.tw@gmail.com>
2020-03-10 18:14:22 +0800

    https://github.com/cube-ui/cube-application-guide

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="header">
+      <div class="title" @click="showPicker">
+        <span>全部赛事</span>
+        <i class="cubeic-select" :class="{flip: toFlip}" ref="select"></i>
+      </div>
+      <div class="navigator">
+        <ul class="nav-list">
+          <li v-for="(item, index) in tabList" :key="index"
+            @click="switchTab(index)" :class="{active: currentPage === index}">
+            {{ item }}
+          </li>
+        </ul>
+        <div class="triangle-up" :class="{left: currentPage === 0, right: currentPage === 2}"></div>
+      </div>
+    </div>
+    <div class="content">
+        <cube-slide
+        :data="tabList"
+        :initialIndex="currentPage"
+        :loop="false"
+        :autoPlay="false"
+        :threshold="0.1"
+        @change="slideChange">
+          <cube-slide-item v-for="(item, index) in tabList" :key="index">
+            <div class="match-list-wrapper">
+              <match-list :type="type" :status="index"></match-list>
+            </div>
+          </cube-slide-item>
+          <div slot="dots"></div>
+      </cube-slide>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MatchList from './components/match-list'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MatchList
+  },
+  data () {
+    return {
+      currentPage: 1,
+      tabList: ['已结束', '直播中', '我的关注'],
+      type: 'soccer'
+    }
+  },
+  methods: {
+    switchTab (index) {
+      this.currentPage = index
+    },
+    slideChange (index) {
+      this.currentPage = index
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+html, body, #app
+  height: 100%
+  text-align: center
+#app
+  background-color: #E0E4E8
+  .header
+    color: white
+    background-color: #15191D
+    .title
+      padding: 20px 0
+      font-size: 16px
+      color: white
+    .navigator
+      position: relative
+      padding-bottom: 12px
+      font-size: 12px
+      .nav-list
+        display: flex
+        justify-content: space-around
+        li
+          width: 60px
+          color: #636873
+          &.active
+            color: white
+    .triangle-up
+      position: absolute
+      left: 50%
+      transform: translate(-50%, 0)
+      bottom: 0
+      width: 0
+      height: 0
+      border-left: 7px solid transparent
+      border-right: 7px solid transparent
+      border-bottom: 8px solid #E0E4E8
+      transition: all 0.4s
+      &.left
+        left: 16.67%
+      &.right
+        left: 83.34%
+  .content
+    height: calc(100% - 80px)
+    overflow: hidden
+    .match-list-wrapper
+      height: 100%
+  
 </style>
